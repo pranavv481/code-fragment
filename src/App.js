d47efa8dafcbbee006857695fdc6dfc805eacf35
pranavv481 <pranavv481@gmail.com>
2020-05-24 01:27:44 +0530

    fragment

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+import FragmentDemo from './components/FragmentDemo';
+import Table from './components/Table';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <FragmentDemo />
+        <Table />
+
+      </div>
+    );
+  }
+}
+
+export default App;
